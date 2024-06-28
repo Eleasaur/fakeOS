1fba891bcede0443cb332d3c4b0d7a7d9d65fb28
Eleasaur <eleanorbuonomo@gmail.com>
2024-06-28 16:24:18 +1000

    added start button functionality in js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.querySelector("#time").textContent = initDateTime;
 
 dateTime;
 setInterval(dateTime, 10000);
+
+const startMenu = document.querySelector(".windows_menu_open");
+const startButton = document.querySelector(".windows_menu_start");
+
+startButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  startMenu.classList.toggle("windows_menu_open--hidden");
+  startButton.classList.toggle("windows_menu_start--clicked");
+});
