26712c78fd8f4adb0b59e578463ba9b2ff86ae7d
Eleasaur <eleanorbuonomo@gmail.com>
2024-06-29 20:54:17 +1000

    added calculator functionality

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const calcDisplay = document.querySelector(".app-container_calculator");
 const toDoDisplay = document.querySelector(".app-container_toDoList");
 const rpsDisplay = document.querySelector(".app-container_rps");
 
-//opening window functions
+//opening windows
 calcDesktopIcon.addEventListener("dblclick", (e) => {
   e.preventDefault;
   calcDisplay.classList.remove("app-container_calculator--hidden");
@@ -75,7 +75,7 @@ const closeCalc = document.querySelector(".app-container_top-bar_close-calc");
 const closeToDo = document.querySelector(".app-container_top-bar_close-toDo");
 const closeRps = document.querySelector(".app-container_top-bar_close-rps");
 
-//closing windows functions
+//closing windows
 closeCalc.addEventListener("click", (e) => {
   e.preventDefault;
   calcDisplay.classList.add("app-container_calculator--hidden");
@@ -92,6 +92,97 @@ closeRps.addEventListener("click", (e) => {
 });
 
 //Calculator app
+class Calculator {
+  constructor(prevOpText, currOpText) {
+    this.prevOpText = prevOpText;
+    this.currOpText = currOpText;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
+  updateDisplay() {
+    this.currOpText.innerText = this.currentOperand;
+    this.prevOpText.innerText = this.previousOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-all-clear]");
+const prevOpText = document.querySelector("[data-prev-op]");
+const currOpText = document.querySelector("[data-curr-op]");
+
+const calculator = new Calculator(prevOpText, currOpText);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+clearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
 //To do list app
 const form = document.querySelector(".toDoListForm");
