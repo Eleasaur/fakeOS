64444a5030b851dd930b586477a34ad2aed49cca
Eleasaur <eleanorbuonomo@gmail.com>
2024-06-29 12:03:17 +1000

    added working desktop icons and close buttons on windows

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.querySelector("#time").textContent = initDateTime;
 dateTime;
 setInterval(dateTime, 10000);
 
+//menu button functionality
 const startMenu = document.querySelector(".windows_menu_open");
 const startButton = document.querySelector(".windows_menu_start");
 
@@ -17,4 +18,48 @@ startButton.addEventListener("click", (e) => {
   e.preventDefault();
   startMenu.classList.toggle("windows_menu_open--hidden");
   startButton.classList.toggle("windows_menu_start--clicked");
+});
+
+//desktop icons opening respective app
+const calcDesktopIcon = document.querySelector("#calculatorShortcut");
+const toDoDesktopIcon = document.querySelector("#toDoListShortcut");
+const rpsDesktopIcon = document.querySelector("#paperScissorsRockShortcut");
+
+const calcDisplay = document.querySelector(".app-container_calculator");
+const toDoDisplay = document.querySelector(".app-container_toDoList");
+const rpsDisplay = document.querySelector(".app-container_rps");
+
+calcDesktopIcon.addEventListener("dblclick", (e) => {
+  e.preventDefault;
+  calcDisplay.classList.remove("app-container_calculator--hidden");
+});
+
+toDoDesktopIcon.addEventListener("dblclick", (e) => {
+  e.preventDefault;
+  toDoDisplay.classList.remove("app-container_toDoList--hidden");
+});
+
+rpsDesktopIcon.addEventListener("dblclick", (e) => {
+  e.preventDefault;
+  rpsDisplay.classList.remove("app-container_rps--hidden");
+});
+
+//close buttons on respective apps
+const closeCalc = document.querySelector(".app-container_top-bar_close-calc");
+const closeToDo = document.querySelector(".app-container_top-bar_close-toDo");
+const closeRps = document.querySelector(".app-container_top-bar_close-rps");
+
+closeCalc.addEventListener("click", (e) => {
+  e.preventDefault;
+  calcDisplay.classList.add("app-container_calculator--hidden");
+});
+
+closeToDo.addEventListener("click", (e) => {
+  e.preventDefault;
+  toDoDisplay.classList.add("app-container_toDoList--hidden");
+});
+
+closeRps.addEventListener("click", (e) => {
+  e.preventDefault;
+  rpsDisplay.classList.add("app-container_rps--hidden");
 });
